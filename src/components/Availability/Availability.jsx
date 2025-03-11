e2d040093a59379bd82452e4d91c4847d6eec032
sam <samuelbretiere@orange.fr>
2025-03-12 00:24:28 +0100

    Correct little CSS conflicts

--- src/components/Availability/Availability.jsx
+++ src/components/Availability/Availability.jsx
@@ -348,7 +348,7 @@ const Availability = ({ startDate, endDate }) => {
                                             isLoading={isLoading}
                                         />
                                     </td>
-                                    <td className="player-name">{player ? player.fullName : ''}</td>
+                                    <td className="player-name-availability">{player ? player.fullName : ''}</td>
                                     {role === 2 && <PlayerTooltip className="" player={player} table={true} />}
                                     {Array.from({length: 3}).map((_, timeSlotIndex) => {
                                         const playerAvailability = playerAvailabilities.find(availability => availability.timeSlot === timeSlotIndex);
