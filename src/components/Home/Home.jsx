b0159646f97e8fdf147e0e68f62dab0b560f62ed
sam <samuelbretiere@orange.fr>
2025-05-24 15:32:03 +0200

    Add ? to avoid null

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -283,7 +283,7 @@ const Home = ({ startDate, endDate, defaultDate }) => {
         if(viewProfile !== ADMIN) return;
         if (match.finish) return (<td className="schedule-actions"></td>);
         if(player === 1) return (<AvailableTooltip className={getColorClassName(match.finish, match.player1Availability, match.player1.id, match.hour)} match={match} handlePlayerAvailability={handlePlayerAvailability} AVAILABLE={AVAILABLE} UNAVAILABLE={UNAVAILABLE} NO_ANSWER={NO_ANSWER}/>);
-        return (<AvailableTooltip className={getColorClassName(match.finish, match.player2Availability, match.player2.id, match.hour)} match={match} handlePlayerAvailability={handlePlayerAvailability} AVAILABLE={AVAILABLE} UNAVAILABLE={UNAVAILABLE} NO_ANSWER={NO_ANSWER}/>);
+        return (<AvailableTooltip className={getColorClassName(match.finish, match.player2Availability, match.player2?.id, match.hour)} match={match} handlePlayerAvailability={handlePlayerAvailability} AVAILABLE={AVAILABLE} UNAVAILABLE={UNAVAILABLE} NO_ANSWER={NO_ANSWER}/>);
     }
 
     const putPlayerTooltip = (match, player) => {
@@ -291,7 +291,7 @@ const Home = ({ startDate, endDate, defaultDate }) => {
         if(match.double && player === 1) return (<TeamTooltip className={getColorClassName(match.finish, match.player1Availability, match.player1.id, match.hour)} team={match.player1} table={true} />);
         if(match.double && player === 2) return (<TeamTooltip className={getColorClassName(match.finish, match.player2Availability, match.player2.id, match.hour)} team={match.player2} table={true} />);
         if(player === 1) return (<PlayerTooltip className={getColorClassName(match.finish, match.player1Availability, match.player1.id, match.hour)} player={match.player1} table={true} />);
-        return (<PlayerTooltip className={getColorClassName(match.finish, match.player2Availability, match.player2.id, match.hour)} player={match.player2} table={true} />);
+        return (<PlayerTooltip className={getColorClassName(match.finish, match.player2Availability, match.player2?.id, match.hour)} player={match.player2} table={true} />);
     }
 
     const showSwitch = () => {
@@ -362,10 +362,10 @@ const Home = ({ startDate, endDate, defaultDate }) => {
                         <tr className={getMatchClassName(match)} key={match}>
                             <td className="schedule-col-hour">{match.hour}</td>
                             {getCourt(match.court)}
-                            <td className={`${getPlayerClassName()} ${getColorClassName(match.finish, match.player1Availability, match.player1.id, match.hour)}`} colSpan={viewProfile === ADMIN ? 1 : 3}>{match.player1.fullName} ({match.player1.ranking})</td>
+                            <td className={`${getPlayerClassName()} ${getColorClassName(match.finish, match.player1Availability, match.player1?.id, match.hour)}`} colSpan={viewProfile === ADMIN ? 1 : 3}>{match.player1?.fullName} ({match.player1?.ranking})</td>
                             {putAvailableTooltip(match, handlePlayer1Availability, 1)}
                             {putPlayerTooltip(match, 1)}
-                            <td className={`${getPlayerClassName()} ${getColorClassName(match.finish, match.player2Availability, match.player2.id, match.hour)}`} colSpan={viewProfile === ADMIN ? 1 : 3}>{match.player2.fullName} ({match.player2.ranking})</td>
+                            <td className={`${getPlayerClassName()} ${getColorClassName(match.finish, match.player2Availability, match.player2?.id, match.hour)}`} colSpan={viewProfile === ADMIN ? 1 : 3}>{match.player2?.fullName} ({match.player2?.ranking})</td>
                             {putAvailableTooltip(match, handlePlayer2Availability, 2)}
                             {putPlayerTooltip(match, 2)}
                             {matchResult(match)}
